1689db5a373d456f8c4ab9c9be83054c2183b799
shoheigold <shoheigold@gmail.com>
2019-10-07 14:58:54 +0900

    長谷部アプリの状態

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <assets_audio_player/AssetsAudioPlayerPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AssetsAudioPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"AssetsAudioPlayerPlugin"]];
+}
+
+@end
